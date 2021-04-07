e0504d78a6b189e463831a1f855d10edc57a6262
a2uuz <aziznageeye9@gmail.com>
2021-04-07 21:52:57 +0300

    variable reassignment

---
+++ variable reassignment.php
@@ -0,0 +1,15 @@
+<?php
+  
+// Add your code here:
+
+$movie = "it";
+
+  echo "I'm a fickle person, my favorite movie used to be $movie.";
+  
+// Add a statement here:
+  $movie = "enception";
+  
+  echo "\nBut now my favorite is $movie.";
+  
+// Add a statement below:
+$old_favorite = $movie;
